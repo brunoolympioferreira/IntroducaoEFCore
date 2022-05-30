35a9e246e797c6a81af05980ea500542c5cd914c
Bruno Olympio <b.olympio@reply.com>
2022-05-30 15:44:40 -0300

    respositorio do curso

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,100 @@
+﻿using CursoEFCore.Data.Configurations;
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace CursoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server=localhost;Database=CursoEF;User Id=sa;Password=1234;",
+                    p => p.EnableRetryOnFailure(
+                        maxRetryCount: 2, 
+                        maxRetryDelay: TimeSpan.FromSeconds(5),
+                        errorNumbersToAdd: null).MigrationsHistoryTable("curso_ef_core"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Modelo de Mapeamento dentro da Classe
+            //modelBuilder.Entity<Cliente>(p =>
+            //{
+            //    p.ToTable("Clientes");
+            //    p.HasKey(p => p.Id);
+            //    p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+            //    p.Property(P => P.Telefone).HasColumnType("CHAR(11)");
+            //    p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
+            //    p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+            //    p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+
+            //    p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
+            //});
+
+            //modelBuilder.Entity<Produto>(p =>
+            //{
+            //    p.ToTable("Produtos");
+            //    p.HasKey(p => p.Id);
+            //    p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
+            //    p.Property(p => p.Descricao).HasColumnType("VARCHAR(60)");
+            //    p.Property(p => p.Valor).IsRequired();
+            //    p.Property(p => p.TipoProduto).HasConversion<string>();
+            //});
+
+            //modelBuilder.Entity<Pedido>(p =>
+            //{
+            //    p.ToTable("Pedidos");
+            //    p.HasKey(p => p.Id);
+            //    p.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
+            //    p.Property(p => p.Status).HasConversion<string>();
+            //    p.Property(p => p.TipoFrete).HasConversion<int>();
+            //    p.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
+
+            //    p.HasMany(p => p.Itens)
+            //        .WithOne(p => p.Pedido)
+            //        .OnDelete(DeleteBehavior.Cascade);
+            //});
+
+            //modelBuilder.Entity<PedidoItem>(p =>
+            //{
+            //    p.ToTable("PedidoItens");
+            //    p.HasKey(p => p.Id);
+            //    p.Property(p => p.Quantidade).HasDefaultValue(1).IsRequired();
+            //    p.Property(p => p.Valor).IsRequired();
+            //    p.Property(p => p.Desconto).IsRequired();
+            //});
+            #endregion
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+
+                foreach (var property in properties)
+                {
+                    if (string.IsNullOrEmpty(property.GetColumnType())
+                         && !property.GetMaxLength().HasValue)
+                    {
+                        property.SetColumnType("VARCHAR(100)");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Curso/Program.cs
@@ -0,0 +1,195 @@
+﻿using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CursoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using var db = new Data.ApplicationContext();
+
+            //var existe = db.Database.GetPendingMigrations().Any();
+            //if (existe)
+            //{
+            //    // aplicar uma regra de negocio
+            //}
+
+            //InserirDados();
+            //InserirDadosEmMassa();
+            //ConsultarDados();
+            //CadastrarPedido();
+            //ConsultarPedidoCarregametoAdiantado();
+            //AtualizarDados();
+            RemoverRegistros();
+        }
+        private static void RemoverRegistros()
+        {
+            using var db = new Data.ApplicationContext();
+            //var cliente = db.Clientes.Find(2); // metodo conectado
+            var cliente = new Cliente { Id = 3 }; // metodo desconectado
+            //db.Clientes.Remove(cliente);
+            //db.Remove(cliente);
+            db.Entry(cliente).State = EntityState.Deleted;
+
+            db.SaveChanges();
+
+        }
+        private static void AtualizarDados()
+        {
+            using var db = new Data.ApplicationContext();
+            //var cliente = db.Clientes.Find(1);
+
+            var cliente = new Cliente
+            {
+                Id = 1
+            };
+
+            var clienteDesconectado = new
+            {
+                Nome = "Cliente Desconectado Passo 3",
+                Telefone = "666333222"
+            };
+
+            db.Attach(cliente);
+            db.Entry(cliente).CurrentValues.SetValues(clienteDesconectado);
+
+            //cliente.Nome = "Cliente alterado passo 2";
+            //db.Clientes.Update(cliente);
+            db.SaveChanges();
+        }
+        private static void ConsultarPedidoCarregametoAdiantado()
+        {
+            using var db = new Data.ApplicationContext();
+            var pedidos = db.Pedidos
+                .Include(p => p.Itens)
+                    .ThenInclude(p => p.Produto)
+                .ToList();
+
+            Console.WriteLine(pedidos.Count);
+        }
+
+        private static void CadastrarPedido()
+        {
+            using var db = new Data.ApplicationContext();
+
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Observacao = "Pedido Teste",
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>
+                {
+                    new PedidoItem
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10,
+                    }
+                }
+            };
+
+            db.Pedidos.Add(pedido);
+
+            db.SaveChanges();
+        }
+
+        private static void ConsultarDados()
+        {
+            using var db = new Data.ApplicationContext();
+            //var ConsultarPorSintaxe = (from c in db.Clientes where c.Id>0 select c).ToList();
+            var consultaPorMetodo = db.Clientes
+                .Where(p => p.Id > 0)
+                .OrderBy(p => p.Id)
+                .ToList();
+            foreach (var cliente in consultaPorMetodo)
+            {
+                Console.WriteLine($"Consultando Cliente: {cliente.Id}");
+                //db.Clientes.Find(cliente.Id);
+                db.Clientes.FirstOrDefault(p => p.Id == cliente.Id);
+            }
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Bruno Ferreira",
+                CEP = "99999000",
+                Cidade = "Santo Andre",
+                Estado = "SP",
+                Telefone = "988526512",
+            };
+
+            var listaClientes = new[]
+            {
+                new Cliente
+                {
+                    Nome = "Teste 1",
+                    CEP = "99999000",
+                    Cidade = "Santo Andre",
+                    Estado = "SP",
+                    Telefone = "988526512",
+                },
+                new Cliente
+                {
+                    Nome = "Teste 2",
+                    CEP = "99999000",
+                    Cidade = "Santo Andre",
+                    Estado = "SP",
+                    Telefone = "988526512"
+                }
+            };
+
+
+            using var db = new Data.ApplicationContext(); 
+            //db.AddRange(produto, cliente);
+            //db.Clientes.AddRange(listaClientes);
+            db.Set<Cliente>().AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.Produtos.Add(produto);
+            //db.Set<Produto>().Add(produto);
+            //db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros} ");
+        }
+    }
+}
